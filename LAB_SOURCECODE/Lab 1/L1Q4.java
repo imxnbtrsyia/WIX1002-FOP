23c5d7bece0405a743545d3b458585e36f689108
imxnbtrsyia <168093144+imxnbtrsyia@users.noreply.github.com>
2025-11-04 21:46:49 +0800

    Lab 1 source code

---
+++ LAB_SOURCECODE/Lab 1/L1Q4.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fop_l1;
+
+/**
+ *
+ * @author Shasha
+ */
+public class L1Q4 {
+    public static void main(String[] args) {
+        System.out.println("======== Total Sales Of Product A in 2016 =======");
+        System.out.println("    Sales");
+        System.out.println("     ^");
+        System.out.println("3500 +");
+        System.out.println("     |");
+        System.out.println("     |                        x");
+        System.out.println("     |                        |");
+        System.out.println("3000 +                        |");
+        System.out.println("     |                        |");
+        System.out.println("     |                   x    |");
+        System.out.println("     |                   |    |");
+        System.out.println("2500 +    x              |    |");
+        System.out.println("     |    |              |    |");
+        System.out.println("     |    |              |    |");
+        System.out.println("     |    |              |    |");
+        System.out.println("2000 +    |         x    |    |");
+        System.out.println("     |    |         |    |    |");
+        System.out.println("     |    |         |    |    |");
+        System.out.println("     |    |    x    |    |    |");
+        System.out.println("1500 +    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("1000 +    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println(" 950 +    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println(" 900 +    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println(" 850 +    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |");
+        System.out.println(" 800 +    |    |    |    |    |    x");
+        System.out.println("     |    |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |    |");
+        System.out.println("     |    |    |    |    |    |    |");
+        System.out.println("     +----+----+----+----+----+----+----> Month");
+        System.out.println("         Jan  Feb  Mar  Apr  May  Jun");
+        
+    } 
+}
---
+++ LAB_SOURCECODE/Lab 1/MyProfile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fop_l1;
+
+/**
+ *
+ * @author Shasha
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Full Name     : Nur Iman Batrisyia HR");
+        System.out.println("Matric Number : 23001812/2");
+        System.out.println("Address       : Kuala Lumpur, Malaysia");
+        System.out.println("Email         : 23001812@siswa.um.edu.my");
+        System.out.println("Contact       : 012-3456789");
+        System.out.println("Programme     : Bachelor of Computer Science (Data Science)");
+    }
+}
+
+  
